4959bc8551416dc199da337751c37db16ae893e0
Anil Singh <anil05.me@gmail.com>
2025-01-08 18:53:58 -0600

    Initial Commits

---
+++ src/test/java/com/example/springai/spring_ai_with_gemini/SpringAiWithGeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springai.spring_ai_with_gemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiWithGeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
