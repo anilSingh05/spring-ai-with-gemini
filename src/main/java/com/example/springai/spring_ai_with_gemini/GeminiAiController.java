715cb59e8999b5c12a096c1f323988b6a83fdc30
Anil Singh <anil05.me@gmail.com>
2025-01-08 21:00:06 -0600

    Setup connectivity with Google Gemini

---
+++ src/main/java/com/example/springai/spring_ai_with_gemini/GeminiAiController.java
@@ -0,0 +1,24 @@
+package com.example.springai.spring_ai_with_gemini;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/gemini")
+@AllArgsConstructor
+public class GeminiAiController {
+
+    private final QnAService qnAService;
+
+    @PostMapping("/qna-chat-bot")
+    public ResponseEntity<String> qnaChatBotCaller(@RequestBody Map<String, String> payload){
+
+        String question =  payload.get("question");
+        String answer = qnAService.getAnswer(question);
+        return ResponseEntity.ok(answer);
+
+    }
+}
---
+++ src/main/java/com/example/springai/spring_ai_with_gemini/QnAService.java
@@ -0,0 +1,69 @@
+package com.example.springai.spring_ai_with_gemini;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Service
+public class QnAService {
+
+    // Access   to APIKEY  and URL
+    @Value("${gemini.ai.api-url}")
+    private String geminiApiUrl;
+    @Value("${gemini.ai.api-key}")
+    private String geminiApikey;
+
+    private final WebClient webClient;
+
+    public QnAService(WebClient.Builder webClient) {
+        this.webClient = webClient.build();
+    }
+
+    /**
+     *
+     * To  Build Request body
+     * Call  Gemini API
+     * And send response back
+     *
+     *
+     * @param question
+     * @return
+     *
+     * {
+     *     "contents": [
+     *         {
+     *             "parts": [
+     *                 {
+     *                     "text": "Explain how AI works"
+     *                 }
+     *             ]
+     *         }
+     *     ]
+     * }
+     */
+    public String getAnswer(String question) {
+
+        // Construct  the request payload
+        Map<String, Object> requestBody =  Map.of("contents", new Object[]{
+                Map.of("parts", new Object[]{
+                        Map.of("text", question)
+                })
+        });
+
+        // Make API  Call
+        String response  = webClient
+                .post()
+                .uri(geminiApiUrl+geminiApikey)
+                .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)  // reactive wrapper
+                .block();
+
+        // Return response
+        return  response;
+    }
+}
